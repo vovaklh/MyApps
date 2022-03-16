29a4306f8a197dc016130da5243ebff60587f3fb
Vladimir <vovaklh20012@gmail.com>
2022-03-16 14:21:54 +0200

    DA-4: Create project structure

---
+++ lib/core/application.dart
@@ -0,0 +1,17 @@
+import 'package:device_apps/presentation/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
