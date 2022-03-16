5f64d40dcbdc8c8ad7c29aa94b194e3deed49b26
Vladimir <vovaklh20012@gmail.com>
2022-03-16 15:38:07 +0200

    DA-5: Setup localization

--- lib/core/application.dart
+++ lib/core/application.dart
@@ -1,5 +1,8 @@
-import 'package:device_apps/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:device_apps/l10n/l10n.dart';
+import 'package:device_apps/presentation/pages/home_page.dart';
 
 class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
@@ -8,6 +11,13 @@ class Application extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      supportedLocales: L10n.all,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
